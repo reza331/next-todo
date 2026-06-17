9673a089d31bce1a0a1ce3c003c92b67c10daf18
reza331 <reza331wn@gmail.com>
2026-06-17 21:15:51 +0330

    middleware removed

--- src/app/( user panel )/profile/page.js
+++ src/app/( user panel )/profile/page.js
@@ -1,7 +1,13 @@
 import ProfileSettings from '@/components/Templates/ProfilePage/ProfileSettings'
+import { checkUser } from '@/utils/auth/checkUser'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 export default async function Profile() {
+
+    const authResult = await checkUser()
+
+    if (authResult.response.status !== 200) redirect('/login')
 
     return (
         <>
